f6619b13254266e38c7b3575a71422ad5998f051
duck-manh-tran <ducmanh.m18@gmail.com>
2024-05-15 13:56:22 +0700

    update analysis control file

--- python/anls_dsch_nwks/program/analysis_dsn.py
+++ python/anls_dsch_nwks/program/analysis_dsn.py
@@ -3,40 +3,64 @@ import os
 import csv
 import matplotlib.pyplot as plt
 
+
 def main():
+
+#	analysis('volt')
+	analysis('temp')
+
+
+
+def analysis(sim_type):
+	if sim_type == 'volt':
+		print('Running analysis of supply voltage variation\n')
+	elif sim_type == 'temp':
+		print('Running analysis of temperature variation\n')
+
 	lib = 'tsmc65_hspice'
 #	lib = 'sky130a_ngspice'
 #	lib = 'sky130a_spectre'	
 
-	model = 'dschr_nwk_type_a'
+	model = 'dschr_nwk_type_h'
+	temps = np.arange(-40, 101, 10)
 	vdds = np.arange(0.4, 0.81, 0.05)
 	inits = np.arange(0.3, 1.01, 0.1)
 
-#	simulation(model, 0.8, 0.8, 1000, 110)
-
-	if (os.path.exists('results/' + lib + '/' + model + '.txt')):
+	if (os.path.exists('results/' + sim_type + '/' + lib + '/' + model + '.txt')):
 		print ('load data from result file')
 	else:
 		print ('result file is not exist, run simulations')
 		if (lib == 'tsmc65_hspice'):
-			hspice_sim(model)
+			hspice_sim(model, sim_type)
 		elif (lib == 'sky130a_ngspice'):
 			multi_sims(model, vdds, inits)			
 		elif (lib == 'sky130a_spectre'):
-			print ('spectre is running')
-	show_graph(lib, model, vdds, inits)
-	plt.show()
+			spectre_sim()
 
+	if sim_type	== 'volt':
+		show_volt_graph(lib, model, vdds, inits)
+	elif sim_type == 'temp':
+		show_temp_graph(lib, model, temps, inits)
+	plt.show()
 
 def spectre_sim():
 	print ('spectre')
 
-def hspice_sim (model):
+def hspice_sim (model, sim_type):
 	lib = 'tsmc65_hspice'
+	ctrl_volt_file = 'netlists/tsmc65_hspice/ctrl_volt.spice'
+	ctrl_temp_file = 'netlists/tsmc65_hspice/ctrl_temp.spice'
+	ctrl_sims_file = 'netlists/tsmc65_hspice/ctrl_sims.spice'
+
+	if sim_type == 'volt':
+		os.system ('cp ' + ctrl_volt_file + ' ' + ctrl_sims_file)
+	elif sim_type == 'temp':
+		os.system ('cp ' + ctrl_temp_file + ' ' + ctrl_sims_file)
+
 	cmd = 'hspice '
 	inpfile = 'netlists/' + lib + '/' + model + '_tb.spice'
-	outfile = 'results/' + lib + '/' + model + '.txt'
-	rawfile = 'results/' + lib + '/raw/' + model + '_tb'
+	outfile = 'results/' + sim_type + '/' + lib + '/' + model + '.txt'
+	rawfile = 'results/' + sim_type + '/' + lib + '/raw/' + model + '_tb'
 	arg1 = " -i " + inpfile
 	arg2 = " -o " + rawfile
 	cmd = cmd + arg1 + arg2
@@ -57,8 +81,7 @@ def hspice_sim (model):
 
 	array = np.array(array)
 	arr = np.vstack ((array[:,1], array[:,2], array[:, 3])).T
-	np.savetxt(outfile, arr)	
-
+	np.savetxt(outfile, arr, fmt = '%3.3e')
 
 def get_sim_time(model):
 	vdd = 0.4
@@ -67,11 +90,10 @@ def get_sim_time(model):
 	stop = 100
 	
 	os.system ('rm tmp.txt')
-	simulation(model, vdd, v_init, step, stop)
+	ngspice_sim(model, vdd, v_init, step, stop)
 	data = np.loadtxt('tmp.txt', dtype=float)
 	sim_time = data[2];
 	print (data)
-
 
 def multi_sims(model, vdds, inits):
 	stop = 105		#ms
@@ -79,17 +101,46 @@ def multi_sims(model, vdds, inits):
 	os.system ('rm tmp.txt')
 	for vdd in vdds:
 		for init in inits:
-			simulation(model, vdd, vdd*init, step, stop)
-	cmd = 'cp tmp.txt results/' + model + '.txt'
+			ngspice_sim(model, vdd, vdd*init, step, stop)
+	cmd = 'cp tmp.txt results/volt/' + model + '.txt'
 	os.system(cmd)	
 
-
-def show_graph(lib, model, vdds, inits):
-	filename_ = 'results/' + lib + '/' + model + '.txt'
+def import_data(sim_type, lib, model, vars_1, vars_2):
+	filename_ = 'results/' + sim_type + '/' + lib + '/' + model + '.txt'
 	data = np.loadtxt(filename_, dtype=float)
 	print (data)
-	data3d = data.reshape((len(vdds), len(inits), 3))
+	data3d = data.reshape((len(vars_1), len(vars_2), 3))
 	print (data3d)	
+	return data3d
+
+def show_temp_graph(lib, model, temps, inits):
+	data3d = import_data('temp', lib, model, inits, temps)
+
+	t2_dis = data3d[len(inits)-1, :, 2]
+	for ix in range(len(inits)-2):
+		t1_dis = data3d[ix, :, 2]
+		t_dis = t1_dis * 1e3
+		plt.figure(1)
+		plt.subplot(2, 1, 1)
+		plt.semilogy(temps, t_dis)	
+		
+		plt.subplot(2, 1, 2)
+		plt.plot(temps, t2_dis/t1_dis)
+	
+
+	plt.subplot(2, 1, 1)
+	plt.title('Discharge time analysis of the ' + model + ' ' + lib)
+	plt.xlabel('temperatures')
+	plt.ylabel('Discharge time t1 (ms)')
+
+	plt.subplot(2, 1, 2)
+	plt.xlabel('temperatures')
+	plt.ylabel('Discharge ratio t1/t2')
+	plt.tight_layout()
+	
+
+def show_volt_graph(lib, model, vdds, inits):
+	data3d = import_data('volt', lib, model, vdds, inits)
 	for ix in range(len(vdds)):
 		t_dis = data3d [ix,:,2]
 		t_dis = t_dis * 1e3
@@ -100,7 +151,7 @@ def show_graph(lib, model, vdds, inits):
 	plt.xlabel('V_init / VDD')
 	plt.ylabel('Discharge time (ms)')
 
-def simulation (model, vdd, v_init, step, stop):
+def ngspice_sim(model, vdd, v_init, step, stop):
 	cmd = 'ngspice '
 	model = ' netlists/sky130a_ngspice/' + model + '_tb.spice'
 	arg1 = " -D vdd=" + str(vdd)
