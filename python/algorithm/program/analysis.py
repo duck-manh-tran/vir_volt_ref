8739b48ade717546b229cc7a6e3cefcf774b9705
duck-manh-tran <ducmanh.m18@gmail.com>
2024-06-09 14:50:39 +0700

    update work 20240609

--- python/algorithm/program/analysis.py
+++ python/algorithm/program/analysis.py
@@ -4,7 +4,7 @@ import csv
 import matplotlib.pyplot as plt
 import math
 import time
-
+import pandas as pd
 
 ## option flags to run the analysis
 
@@ -27,8 +27,8 @@ def main():
 	dsnwk = 'dsn_h_12t'
 
 
-	analysis(sim_type, lib, dsnwk)
-
+#	analysis(sim_type, lib, dsnwk)
+	CTAT_var_anls(lib, dsnwk)
 
 
 def analysis(sim_type, lib, dsnwk):
@@ -83,13 +83,11 @@ def hspice_sim (sim_type='volt', lib='tsmc65_hspice', dsnwk='dsn_h_12t', subfix=
 	with open(rawfile, 'r') as csvfile:
 		tmp = csv.reader(csvfile)
 		num_of_val = len(list(tmp)[10])
-		print ('number of values: ', num_of_val)
+		print ('bnumber of values: ', num_of_val)
 	
 	with open(rawfile, 'r') as csvfile:
 		reader = csv.reader(csvfile)
 		for row in reader:
-			print ('debug point')
-			print ('length of row: ', len(row))
 			if ((len(row) == num_of_val) & (row[0] != 'index')):
 				val = np.array(row)
 				val = val.astype(np.float)
@@ -195,8 +193,8 @@ def temp_anls_1(lib, dsnwk):
 		list_obj[5].legend()	
 		list_obj[5].grid()	
 	
-	figManager = plt.get_current_fig_manager()
-	figManager.window.showMaximized()	
+#	figManager = plt.get_current_fig_manager()
+#	figManager.window.showMaximized()	
 	
 	fig.tight_layout(pad=0, w_pad=0, h_pad=0)
 
@@ -205,82 +203,76 @@ def CTAT_var_anls(lib, dsnwk):
 	sim_type = 'temp'
 	filename_ = 'results/' + sim_type + '/' + lib + '/' + dsnwk + '.txt'
 	data = np.loadtxt(filename_, dtype=float)
-	vdds = np.arange(0.4, 0.81, 0.1)
-	temps = np.arange(0, 101, 2)
-	r_caps = np.arange(1.25, 2.501, 0.025)
- 
-	data_as_vdd = data.reshape(len(vdds), len(temps)*len(r_caps) ,len(data[0]))
-
-	def variable_anls(n_vals):
-		for n_val in n_vals:
-			d_12 = n_val[0] - n_val[1]
-			d_23 = n_val[1] - n_val[2]
-			d_13 = n_val[0] - n_val[2]
-			diffs.append([d_12, d_23, d_13])
-			
-			div_1 = d_12/d_23
-			div_2 = d_13/d_23
-			div_3 = d_13/d_12
-			divs.append([div_1, div_2, div_3])
 	
-			y_1 = div_1 + div_2 - 2
-			y_2 = div_1 + div_2 + div_3 - 4
-			y_3 = div_1 + div_2 - div_3
-			y_vals.append([y_1, y_2, y_3])
+	df_cols = ['temp', 'vdd', 'r_cap', 'n1', 'n2', 'n3']
+	raw_df =  pd.DataFrame(data, columns = df_cols)
+
+	def variable_gen(df):
+		df['d_12'] = df['n1'] - df['n2']
+		df['d_23'] = df['n2'] - df['n3']
+		df['d_13'] = df['n1'] - df['n3']
+	
+		df['div_1'] = df.d_12/df.d_23
+		df['div_2'] = df.d_13/df.d_23
+		df['div_3'] = df.d_13/df.d_12
+	
+		df['y_1'] = df.div_1 + df.div_2 - 2
+		df['y_2'] = df.div_1 + df.div_2 + df.div_3 - 4
+		df['y_3'] = df.div_1 + df.div_2 - df.div_3
 		
-			ey_1 = np.exp(y_1)
-			ey_2 = np.exp(y_2)
-			ey_3 = np.exp(y_3)
-			ey_vals.append([ey_1, ey_2, ey_3])
-	
-		diffs = np.array(diffs)
-		divs = np.array(divs)
-		y_vals = np.array(y_vals)
-		ey_vals = np.array(ey_vals)
-		return diffs, divs, y_vals, ey_vals
+		df['ey_1'] = np.exp(df.y_1)
+		df['ey_2'] = np.exp(df.y_2)
+		df['ey_3'] = np.exp(df.y_3)
+		return df
 
+	processed_df = variable_gen(raw_df)
+	filt_df = processed_df[processed_df['vdd'] == 0.4]
 	
-	n_vals_vdd_8 = data_as_vdd[4, :, 3:6] * 1e6		# n_vals with vdd = 0.8
-	temps = data_as_vdd[4, :, 0] 	
-	r_caps = data_as_vdd[4, : 2] 	
-	diffs, divs, y_vals, ey_vals = variable_anls(n_vals_vdd_8) 
-	
+	r_caps = [1.3, 1.6, 1.9, 2.1, 2.4]
 	list_obj = []
-	fig2 = plt.figure('temp_var_anls')
+	fig = plt.figure('temp_var_anls')
 	n_rows = 2
 	n_cols = 3
-	colors = ['navy', 'blue', 'dodgerblue']
-	
-	list_obj.append(fig2.add_subplot(n_rows, n_cols, 1, projection='3d'))
-	names = ['$n_1$', '$n_2$', '$n_3$']
-	for idd in range(3):
-		list_obj[0].scatter(temps, r_caps, n_vals.T[ix], c=colors[ix], label=names[ix])
+#	colors = ['navy', 'blue', 'dodgerblue']
+	colors = ['blue', 'orange', 'green', 'red', 'purple']	
 
-#	list_obj.append(fig2.add_subplot(n_rows, n_cols, 2, projection='3d'))
-#	names = ['$d_{12}=n_1-n_2$', '$d_{23}=n_2-n_3$', '$d_{13}=n_1-n_3$']
-#	for ix in range(3):
-#		list_obj[1].scatter(diffs.T[ix], r_caps, r_reals, c=colors[ix], label=names[ix])
-#
-#	list_obj.append(fig2.add_subplot(n_rows, n_cols, 3, projection='3d'))
-#	names = ['$dv_1=d_{12}/d_{23}$', '$dv_2=d_{13}/d_{23}$', '$dv_3=d_{13}/d_{12}$']
-#	for ix in range(3):
-#		list_obj[2].scatter(divs.T[ix], r_caps, r_reals, c=colors[ix], label=names[ix])
-#
-#	list_obj.append(fig2.add_subplot(n_rows, n_cols, 4, projection='3d'))
-#	names = ['$y_1=dv_1+dv_2-2$', '$y_2=dv_1+dv_2+dv_3-4$', '$y_3=dv_1+dv_2-dv3$']
-#	for ix in range(3):
-#		list_obj[3].scatter(y_vals.T[ix], r_caps, r_reals, c=colors[ix], label=names[ix])
-#
-#	list_obj.append(fig2.add_subplot(n_rows, n_cols, 5, projection='3d'))
-#	names = ['$e^{y_1}$', '$e^{y_2}$', '$e^{y_3}$']
-#	for ix in range(3):
-#		list_obj[4].scatter(ey_vals.T[ix], r_caps, r_reals, c=colors[ix], label=names[ix])
+	list_obj.append(fig.add_subplot(n_rows, n_cols, 1, projection='3d'))
+	names = ['$n_1$', '$n_2$', '$n_3$']
+	cols = ['n1', 'n2', 'n3']
+	for ix in range(3):
+		list_obj[0].scatter(filt_df['temp'], filt_df['r_cap'], filt_df[cols[ix]], \
+				c=colors[ix], label=names[ix])
+
+	list_obj.append(fig.add_subplot(n_rows, n_cols, 2, projection='3d'))
+	names = ['$d_{12}=n_1-n_2$', '$d_{23}=n_2-n_3$', '$d_{13}=n_1-n_3$']
+	cols = ['d_12', 'd_23', 'd_13']
+	for ix in range(3):
+		list_obj[1].scatter(filt_df['temp'], filt_df['r_cap'], filt_df[cols[ix]], \
+				c=colors[ix], label=names[ix])
+
+	filt_rcap_mk  = filt_df['r_cap'].isin(r_caps)
+	filt_rcap  = filt_df[filt_rcap_mk]
+
+	list_obj.append(fig.add_subplot(n_rows, n_cols, 3))
+	names = ['$dv_1=d_{12}/d_{23}$', '$dv_2=d_{13}/d_{23}$', '$dv_3=d_{13}/d_{12}$']
+	cols = ['div_1', 'div_2', 'div_3']
+	for ix in range(3):
+		list_obj[2].scatter(filt_rcap['temp'], filt_rcap[cols[ix]], c=colors[ix], label=names[ix])
+
+	list_obj.append(fig.add_subplot(n_rows, n_cols, 4))
+	names = ['$y_1=dv_1+dv_2-2$', '$y_2=dv_1+dv_2+dv_3-4$', '$y_3=dv_1+dv_2-dv3$']
+	cols = ['y_1', 'y_2', 'y_3']
+	for ix in range(3):
+		list_obj[3].scatter(filt_rcap['temp'], filt_rcap[cols[ix]], c=colors[ix], label=names[ix])
+
+	list_obj.append(fig.add_subplot(n_rows, n_cols, 5))
+	list_obj[4].scatter(filt_rcap['temp'], filt_rcap['div_3'], c=colors[0], label='$dv_3=d_{13}/d_{12}$')
 
 	for ix in range(5):
 		list_obj[ix].legend()	
 		list_obj[ix].grid(True)	
-
-
+	fig.tight_layout(pad=0, w_pad=0, h_pad=0)
+	plt.show()
 
 
 def volt_anls_1 (lib, dsnwk):
@@ -361,8 +353,8 @@ def volt_anls_1 (lib, dsnwk):
 		list_obj[ix].legend()	
 		list_obj[ix].grid(True)	
 
-	figManager = plt.get_current_fig_manager()
-	figManager.window.showMaximized()	
+#	figManager = plt.get_current_fig_manager()
+#	figManager.window.showMaximized()	
 	
 	fig2.tight_layout(pad=0, w_pad=0, h_pad=0)
 
